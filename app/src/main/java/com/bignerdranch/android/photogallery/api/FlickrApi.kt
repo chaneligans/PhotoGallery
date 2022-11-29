aa1a13762b193425c2aa58b57b03d182d2993d82
Chanel Mendoza <chanelmdza@gmail.com>
2022-11-28 20:01:19 -0800

    Update how we get responses back

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -2,8 +2,14 @@ package com.bignerdranch.android.photogallery.api
 
 import retrofit2.http.GET
 
+private const val API_KEY = "07241be946598c789ec9d9c5172b9042"
+
 interface FlickrApi {
 
-    @GET("/")
-    suspend fun fetchContents(): String
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=$API_KEY" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s")
+    suspend fun fetchPhotos(): FlickrResponse
 }
